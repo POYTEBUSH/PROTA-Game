b4f66073867fe33f7d4263e01455afbe05cac065
STUDENT\b013432f <potyebush@gmail.com>
2019-01-25 22:10:51 +0000

    Added base player script

---
+++ Game/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Morale;
+    public float Hunger;
+    public float Cleanliness;
+    public float Warmth;
+
+    public float Money;
+    private int MaxMoneyStore;
+    public string Name;
+
+    public int Level;
+    public float Experience;
+    public int DaysSurvived;
+
+    public bool GameOver;
+
+    //Items list
+    public void Update()
+    {
+        if (Morale <= 0f)
+            GameOver = true;
+
+        Mathf.Clamp(Morale, 0f, 1f);
+        Mathf.Clamp(Hunger, 0f, 1f);
+        Mathf.Clamp(Cleanliness, 0f, 1f);
+        Mathf.Clamp(Warmth, 0f, 1f);
+        Mathf.Clamp(Money, 0f, MaxMoneyStore);
+
+        CheckLevelProgress();
+    }
+
+    private void CheckLevelProgress()
+    {
+        if(Experience >= (Level * 120))
+        {
+            Level++;
+            Experience = 0;
+        }
+    }
+
+    public void SetMaxMoney(int MaxMoney)
+    {
+        Mathf.Clamp(Morale, 0f, 1f);
+    }
+}
