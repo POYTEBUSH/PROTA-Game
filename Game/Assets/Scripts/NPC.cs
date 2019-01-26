110e96001773dcaf6cf4e2b55fa7b7d63ef67259
STUDENT\b015133f <b015133f@student.staffs.ac.uk>
2019-01-26 02:57:23 +0000

    + Added: AI Shizzle

---
+++ Game/Assets/Scripts/NPC.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+    Vector3 startPos = new Vector3(-10.0f, 0.42f, -9.5f);
+    Vector3 velocity = new Vector3(1.0f, 0.0f, 0.0f);
+    Vector3 otherVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+
+    bool facingLeft, staring, paying = false;
+
+    public float minimumTimeToStay, minimumTimeToPay, minimumToPay;
+    
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        facingLeft = (Random.value > 0.5f);
+
+        velocity.x *= (Random.Range(1.0f, 2.0f));
+
+        startPos.z = Random.Range(-10.0f, -6.0f);
+
+        if (facingLeft)
+        {
+            startPos.x *= -1.0f;
+            velocity.x *= -1.0f;
+        }
+
+        transform.localPosition = startPos;
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        rb.velocity = velocity;
+
+        if (transform.localPosition.x * transform.localPosition.x > 110)
+            Destroy(transform.gameObject);
+
+        if (staring && !paying)
+        {
+
+        }
+    }
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("NPC"))
+        {
+            otherVelocity = collision.transform.GetComponent<Rigidbody>().velocity;
+            if (!staring)
+                HandleNPCCollision();
+        }
+        else if (collision.transform.CompareTag("Player"))
+        {
+            HandlePlayerCollision();
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.CompareTag("NPC"))
+        {
+            velocity.z = 0.0f;
+        }
+    }
+
+    private void HandleNPCCollision()
+    {
+        if (velocity.x == 0.0f)
+        {
+            velocity.x = otherVelocity.x;
+            if (velocity.x < 0.0f)
+                facingLeft = true;
+            else
+                facingLeft = false;
+        }
+        else if (facingLeft)
+        {
+            if (otherVelocity.x >= 0.0f)
+                velocity.z = -1.0f;
+            else if (otherVelocity.sqrMagnitude > velocity.sqrMagnitude)
+                velocity.x = otherVelocity.x;
+        }
+        else if (transform.localPosition.z < -6.0f)
+        {
+            if (otherVelocity.x <= 0.0f)
+                velocity.z = 1.0f;
+            else if (otherVelocity.sqrMagnitude > velocity.sqrMagnitude)
+                velocity.x = otherVelocity.x;
+        }
+    }
+
+    private void HandlePlayerCollision()
+    {
+        int reaction = Mathf.CeilToInt(Random.Range(0.01f, 2.99f));
+
+        switch (reaction)
+        {
+            case (1):
+                velocity.x *= -2.0f;
+
+                if (facingLeft)
+                    facingLeft = false;
+                else
+                    facingLeft = true;
+                break;
+            case (2):
+                velocity.x *= 2.0f;
+                break;
+            default:
+                StartCoroutine(StareAtHomeless());
+                break;
+        }
+    }
+
+    IEnumerator StareAtHomeless()
+    {
+        velocity.x = 0.0f;
+        staring = true;
+
+        yield return new WaitForSeconds(Random.Range(minimumTimeToStay, minimumTimeToStay + 10));
+
+        staring = false;
+    }
+
+    IEnumerator PayHomeless()
+    {
+        paying = true;
+
+        yield return new WaitForSeconds(Random.Range(minimumTimeToPay, minimumTimeToPay + 3));
+
+        ///////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////
+        //// CODE TO GIVE MONEY STORED IN minimumToPay TO HOMELESS MAN ////
+        ///////////////////////////////////////////////////////////////////
+        ///// LEAVE IF TO ENSURE NPC IS STILL WATCHING HOMELESS MAN! //////
+        ///////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////
+        if (staring)
+        {
+
+        }
+
+        paying = false;
+    }
+}
---
+++ Game/Assets/Scripts/NPCManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+
+    public GameObject NPC;
+    public float minSecondsBetweenSpawn;
+    public float maxSecondsBetweenSpawn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(SpawnNPC());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator SpawnNPC()
+    {
+        yield return new WaitForSeconds(Random.Range(minSecondsBetweenSpawn, maxSecondsBetweenSpawn));
+
+        Instantiate(NPC);
+
+        StartCoroutine(SpawnNPC());
+    }
+}
