90258cf0db599196eb4ee42e104445cd30ffcb53
Ricky-Lee Williams <w019278f@student.staffs.ac.uk>
2019-01-26 01:43:25 +0000

    + Added: Main Menu UI Elements

---
+++ Game/Assets/Scripts/HideText.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HideText : MonoBehaviour
+{
+    //public GameObject obj;
+    private Text text;
+
+	// Use this for initialization
+	void Start ()
+    {
+       text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Image")
+        {
+            text.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Image")
+        {
+            text.enabled = false;
+        }
+    }
+}
---
+++ Game/Assets/Scripts/LevelChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    public Animator mLevelAnim;
+    private int mLevelToLoad;
+
+    public void FadeToLevl(int level)
+    {
+        mLevelToLoad = level;
+        mLevelAnim.SetTrigger("Fade Out");
+    }
+
+    public void QuitGame ()
+    {
+        #if UNITY_STANDALONE
+            Application.Quit();
+        #endif
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
+    public void ChangeLevel()
+    {
+        SceneManager.LoadScene(mLevelToLoad);
+    }
+}
