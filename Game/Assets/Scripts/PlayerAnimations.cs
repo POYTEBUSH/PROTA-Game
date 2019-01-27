5592097f84c5a76832c40788a9f6d9d9a4a57696
STUDENT\g013895f <griffo.alex@gmail.com>
2019-01-27 15:15:29 +0000

    Added in Hotbar buttons, to merge with sound branch

--- Game/Assets/Scripts/PlayerAnimations.cs
+++ Game/Assets/Scripts/PlayerAnimations.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class PlayerAnimations : MonoBehaviour
 {
     private Animator animator;
+    bool triggerDance = false;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
+        
+
         animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyUp(KeyCode.Alpha1))
-        {
-            animator.SetTrigger("Dance");
-        }
 
         if (Input.GetKeyUp(KeyCode.Alpha2))
         {
@@ -29,5 +28,10 @@ public class PlayerAnimations : MonoBehaviour
         {
             animator.SetTrigger("Drinking");
         }
+    }
+
+    public void TriggerBreakdance()
+    {
+        animator.SetTrigger("Dance");
     }
 }
