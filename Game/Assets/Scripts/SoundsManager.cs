78882e0704115c94a6ee28bb85ec238a45ae14d3
Ricky-Lee Williams <w019278f@student.staffs.ac.uk>
2019-01-27 13:48:02 +0000

    + Added: Working on audio

---
+++ Game/Assets/Scripts/SoundsManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class SoundsManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    // Use this for initialization
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void OnPlayerConnected(string name)
+    {
+       Sound s =  Array.Find(sounds, sound => sound.name == name);
+       s.source
+    }
+}
